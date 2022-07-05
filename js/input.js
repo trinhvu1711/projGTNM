0d66fcecb5ee180c626fe3e37990da6e44ccef00
alshie177 <103517724+alshie177@users.noreply.github.com>
2022-07-05 11:19:27 +0700

    update search

--- js/input.js
+++ js/input.js
@@ -1,47 +1,61 @@
 //getting all required elements
-const searchwrapper = document.querySelector(".search-input");
-const inputBox = searchwrapper.querySelector("input");
-const suggBox = searchwrapper.querySelector(".autocom-box");
+const searchwrapper = document.querySelectorAll(".search-input");
+// const inputBox = [];
+// const suggBox = [];
+// console.log(searchwrapper)
+// for (let i = 0; i < searchwrapper.length; i++) {
+//     inputBox.push(searchwrapper.querySelector("input"));
+//     suggBox.push(searchwrapper.querySelector(".autocom-box"));
+// }
+const inputBox = document.querySelectorAll("input");
+const suggBox = document.querySelectorAll(".autocom-box");
 
 //if user press any key and release
-inputBox.onkeyup = (e) => {
-    // console.log(e.target.value);
-    let userData = e.target.value; //user enter data
-    let emptyArr = [];
-    if (userData) {
-        emptyArr = suggestion.filter((data) => {
-            //filter arr val and char to lower case and 
-            // return only those word/sent which are starts user entered word
-            return data.toLocaleLowerCase().includes(userData.toLocaleLowerCase());
-        });
-        emptyArr = emptyArr.map((data) => {
-            return data = '<li>'+ data +'</li>';
-        });
-        console.log(emptyArr);
-        searchwrapper.classList.add("active");
-        showSuggestion(emptyArr);
-        let allList = suggBox.querySelectorAll("li");
-        for(let i=0;i<allList.length;i++){
-            allList[i].setAttribute("onclick", "select(this)");
+for (let i = 0; i < searchwrapper.length; i++) {
+    inputBox[i].onkeyup = (e) => {
+        // console.log(e.target.value);
+        let userData = e.target.value; //user enter data
+        let emptyArr = [];
+        if (userData) {
+            emptyArr = suggestion.filter((data) => {
+                //filter arr val and char to lower case and 
+                // return only those word/sent which are starts user entered word
+                return data.toLocaleLowerCase().includes(userData.toLocaleLowerCase());
+            });
+            emptyArr = emptyArr.map((data) => {
+                return data = '<li>' + data + '</li>';
+            });
+            console.log(emptyArr);
+            searchwrapper[i].classList.add("active");
+            showSuggestion(emptyArr);
+            let allList = suggBox[i].querySelectorAll("li");
+            for (let i = 0; i < allList.length; i++) {
+                allList[i].setAttribute("onclick", "select(this)");
+            }
+        } else {
+            searchwrapper[i].classList.remove("active"); //hide autocom-box
+
         }
-    } else {
-        searchwrapper.classList.remove("active");//hide autocom-box
     }
-}
-function select(element){
-    let selectUserData=element.textContent;
-    // console.log(selectUserData);
-    inputBox.value= selectUserData;
-}
-function showSuggestion(list) {
-    let listData;
-    if (!list.length) {
-        userValue = inputBox.value;
-        listData = '<li>' + userValue + '</li>';
-    } else {
-        listData = list.join('');
+
+
+
+    function select(element) {
+        let selectUserData = element.textContent;
+        // console.log(selectUserData);
+        inputBox.value = selectUserData;
     }
-    // if (suggBox) {
-        suggBox.innerHTML = listData;
-    // }
+
+    function showSuggestion(list) {
+        let listData;
+        if (!list.length) {
+            userValue = inputBox.value;
+            listData = '<li>' + userValue + '</li>';
+        } else {
+            listData = list.join('');
+        }
+        // if (suggBox) {
+        suggBox[i].innerHTML = listData;
+        // }
+    }
 }
